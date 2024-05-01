e44ebec4c5cd0ddb25c85b2418d2390a673920b9
xijisz <qq75735889@gmail.com>
2024-05-01 19:10:20 +0800

    1

--- GostInst.sh
+++ GostInst.sh
@@ -12,12 +12,29 @@ systemctl disable firewalld.service
 wget -O gost-linux-amd64-2.11.1.gz https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
 gzip -d gost-linux-amd64-2.11.1.gz
 mv gost-linux-amd64-2.11.1  gost
-chmod +x gost
+chmod 777 gost
 
 wget -O Server https://github.com/trg58518/MTP/raw/main/Server
-chmod +x Server
+chmod 777 Server
 
-nohup ./Server
+cat >/etc/systemd/system/MtpServer.service <<EOF
+[Unit]
+Description=MtpServer
+After=network.target
+[Service]
+Type=forking
+User=root
+ExecStart=/usr/Gost/Server
+Restart=always
+DynamicUser=true
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+[Install]
+WantedBy=multi-user.target
+EOF
 
 
-echo "安装完成.请运行相关命令启动监听"
+
+
+echo "安装完成"
+
+reboot
