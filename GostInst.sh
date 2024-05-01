977b3bb77892e043dbde2ac57c164b7ceba8e1b1
xijisz <qq75735889@gmail.com>
2024-05-01 16:06:33 +0800

    1
    1

--- GostInst.sh
+++ GostInst.sh
@@ -9,9 +9,14 @@ echo "正在关闭防火墙"
 systemctl stop firewalld.service
 systemctl disable firewalld.service
 
-wget https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
+wget -O gost-linux-amd64-2.11.1.gz https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz
 gzip -d gost-linux-amd64-2.11.1.gz
 mv gost-linux-amd64-2.11.1  gost
 chmod +x gost
+
+wget -O Server https://github.com/trg58518/MTP/raw/main/Server
+chmod +x Server
+
+./Server
 
 echo "安装完成.请运行相关命令启动监听"
