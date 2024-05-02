930919977dbccfb043d2ccf81fe3ace155efc7e6
xijisz <qq75735889@gmail.com>
2024-05-02 23:30:20 +0800

    1

--- Install.sh
+++ Install.sh
@@ -123,7 +123,7 @@ do_config_mtp(){
 	input_port=443
 	echo -e "自动设置管理端口8888"
 	input_manage_port=8888
-	echo -e "自动设置伪造域名:github.com"
+	echo -e "自动设置伪造域名:www.apple.com"
 	input_domain="azure.microsoft.com"
 	
 	secret=$(gen_rand_hex 32)
